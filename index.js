2b64a303cc1d617ea5242c9ce9820d7cb325b185
Mark George <thingalon@gmail.com>
2021-06-02 13:21:06 +1000

    Lint picking

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const BrowserInterfacePuppeteer = require( './lib/browser-interface-puppeteer' );
 const BrowserInterfaceIframe = require( './lib/browser-interface-iframe' );
-const { generateCriticalCSS, Cancel } = require( './lib/generate-critical-css' );
+const {
+	generateCriticalCSS,
+	Cancel,
+} = require( './lib/generate-critical-css' );
 
 const ErrorClasses = require( './lib/errors' );
 
